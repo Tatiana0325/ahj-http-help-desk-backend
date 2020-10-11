7f6a83ded860f412c21388aa2875feeaae14abf2
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-10-11 18:15:35 +0300

    First commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require("http");
 const path = require("path");
-const Tiket = require("./Tiket");
+const Ticket = require("./Ticket");
 const Koa = require("koa");
 const koaBody = require("koa-body");
 const koaStatic = require("koa-static");
 const app = new Koa();
 
-const public = path.join(__dirname, "/src");
+const public = path.join(__dirname, "/public");
 app.use(koaStatic(public));
 
 app.use(async (ctx, next) => {
@@ -58,31 +58,31 @@ app.use(async (ctx) => {
   switch (method) {
     case "GET":
       if (ctx.request.query.method === "allTickets") {
-        ctx.response.body = await Tiket.getAll();
+        ctx.response.body = await Ticket.getAll();
         ctx.response.status = 200;
       } else if (ctx.request.query.method === "ticketById") {
-        const tiketId = await Tiket.getById(ctx.request.query.id);
-        ctx.response.body = tiketId;
+        const ticketId = await Ticket.getById(ctx.request.query.id);
+        ctx.response.body = ticketId;
       }
       break;
     case "POST":
-      const tiketPost = new Tiket(
+      const ticketPost = new Ticket(
         ctx.request.body.name,
         ctx.request.body.description
       );
-      await tiketPost.save();
+      await ticketPost.save();
       return;
     case "PUT":
       if (ctx.request.body.status) {
-        await Tiket.updateStatus(ctx.request.body.id);
+        await Ticket.updateStatus(ctx.request.body.id);
       } else if (ctx.request.body.descriptionStatus) {
-        await Tiket.updateDescription(ctx.request.body.id);
+        await Ticket.updateDescription(ctx.request.body.id);
       } else {
-        await Tiket.update(ctx.request.body);
+        await Ticket.update(ctx.request.body);
       }
       return;
     case "DELETE":
-      await Tiket.delete(ctx.request.query.id);
+      await Ticket.delete(ctx.request.query.id);
       return;
     default:
       ctx.response.status = 404;
