4c37f765d876dfcacd0e3871556f7b03943b6599
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-10-16 20:33:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,44 +6,39 @@ const koaBody = require("koa-body");
 const koaStatic = require("koa-static");
 const app = new Koa();
 
-const public = path.join(__dirname, "/public");
+const public = path.join(__dirname, '/public');
+
 app.use(koaStatic(public));
 
 app.use(async (ctx, next) => {
-  const origin = ctx.request.get("Origin");
-  if (!origin) {
-    return await next();
-  }
-
-  const headers = { "Access-Control-Allow-Origin": "*" };
-
-  if (ctx.request.method !== "OPTIONS") {
-    ctx.response.set({ ...headers });
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = { ...e.headers, ...headers };
-      throw e;
+    const origin = ctx.request.get('Origin');
+    if (!origin) {
+        return await next();
     }
-  }
 
-  if (ctx.request.get("Access-Control-Request-Method")) {
-    ctx.response.set({
-      ...headers,
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH",
+    const headers = { 'Access-Control-Allow-Origin': '*', };
+    if (ctx.request.method !== 'OPTIONS') {
+        ctx.response.set({...headers});
+        try {
+            return await next();
+        } catch (e) {
+            e.headers = {...e.headers, ...headers};
+            throw e;
+        }
+    }
+
+    if (ctx.request.get('Access-Control-Request-Method')) {
+        ctx.response.set({
+        ...headers,
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
     });
-
-    if (ctx.request.get("Access-Control-Request-Headers")) {
-      ctx.response.set(
-        "Access-Control-Allow-Headers",
-        ctx.request.get("Access-Control-Request-Headers")
-      );
-    }
-
-    ctx.response.status = 204;
-  }
+        if (ctx.request.get('Access-Control-Request-Headers')) {
+            ctx.response.set('Access-Control-Allow-Headers',
+            ctx.request.get('Access-Control-Allow-Request-Headers'));
+        }
+        ctx.response.status = 204; // No content
+        }
 });
-
 app.use(
   koaBody({
     text: true,
